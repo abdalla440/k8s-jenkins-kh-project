4b6815ebe3e45a7dbcac6677b6251fffad2213b5
A-Hannora <ahannora440@gmail.com>
2024-08-04 01:22:01 +0300

    update cluster terraform fiels + update ansible files v2

--- terraform/eks_cluster.tf
+++ terraform/eks_cluster.tf
@@ -13,3 +13,13 @@ resource "aws_eks_cluster" "k8s" {
 
   }
 }
+
+
+resource "aws_eks_addon" "aws-ebs-csi-driver" {
+  cluster_name             = var.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.eks_role.arn
+  depends_on               = [aws_eks_cluster.k8s]
+
+}
+
